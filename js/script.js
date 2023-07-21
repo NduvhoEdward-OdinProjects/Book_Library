9863e443bc27617a94ccc20f6f96da9391525122
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2023-07-21 03:06:12 +0200

    Add functionality to add books

--- js/script.js
+++ js/script.js
@@ -1,34 +1,100 @@
-let bookLibrary = [];
+let myLibrary = [];
 
-// function Book(title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function(){
-//       return `"${this.title}, by ${this.author}, ${this.pages} pages, ${this.read}"`;
-//   }
-// }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-function Book() {
-  this.init = function (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-      return `"${this.title}, by ${this.author}, ${this.pages} pages, ${this.read}"`;
-    }
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
 
-    return this;
+function displayBooks() {
+  const bookContainer = document.getElementById('book-container');
+  bookContainer.innerHTML = '';
+
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const titleElement = document.createElement('span');
+    titleElement.textContent = `Title: ${book.title}`;
+    bookCard.appendChild(titleElement);
+
+    const authorElement = document.createElement('span');
+    authorElement.textContent = `Author: ${book.author}`;
+    bookCard.appendChild(authorElement);
+
+    const pagesElement = document.createElement('span');
+    pagesElement.textContent = `Pages: ${book.pages}`;
+    bookCard.appendChild(pagesElement);
+
+
+    const readElement = document.createElement('span');
+    readElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+    bookCard.appendChild(readElement);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('remove-button');
+    removeButton.setAttribute('data-index', index);
+    bookCard.appendChild(removeButton);
+
+    const toggleReadButton = document.createElement('button');
+    toggleReadButton.textContent = 'Toggle Read';
+    toggleReadButton.classList.add('toggle-read-button');
+    toggleReadButton.setAttribute('data-index', index);
+    bookCard.appendChild(toggleReadButton);
+
+    bookContainer.appendChild(bookCard);
+  });
+}
+
+
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+  displayBooks();
+}
+
+const newBookButton = document.querySelector('.new-book-button');
+console.log(newBookButton);
+
+newBookButton.addEventListener('click', () => {
+  const title = prompt('Enter book title:');
+  const author = prompt('Enter book author:');
+  const pages = prompt('Enter number of pages:');
+  const read = confirm('Has the book been read?');
+
+  if (title && author && pages) {
+    addBookToLibrary(title, author, pages, read);
+    displayBooks();
   }
-}
+});
 
-function addBookToLibrary() {
-  // do stuff here 
-}
+document.getElementById('book-container').addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('remove-button')) {
+    const index = parseInt(target.getAttribute('data-index'));
+    removeBook(index);
+  } else if (target.classList.contains('toggle-read-button')) {
+    const index = parseInt(target.getAttribute('data-index'));
+    toggleReadStatus(index);
+  }
+});
 
-let book1 = Object.create(Book).init("Telecoms Fundamentals","N.E. Ramashia", 566, "True");
-console.log(book1);
+// Manually add some books for demonstration purposes
+addBookToLibrary('Book 1', 'Author 1', 300, true);
+addBookToLibrary('Book 2', 'Author 2', 250, false);
+addBookToLibrary('Book 3', 'Author 3', 400, true);
 
-console.log("qwqwqw")
+// Display the books
+displayBooks();
